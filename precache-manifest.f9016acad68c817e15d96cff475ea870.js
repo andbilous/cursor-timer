17e8e34c5dcba097c80cd31f5dbc7a6e458e2caf
andbilous-digis <andbilous@gmail.com>
2020-04-11 14:16:47 +0300

    Updates

---
+++ precache-manifest.f9016acad68c817e15d96cff475ea870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66b5c22f0c38a37f8cdcc5ea05c2c86",
+    "url": "/cursor-timer/index.html"
+  },
+  {
+    "revision": "c2f5f03a89d8d86fbcd7",
+    "url": "/cursor-timer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0fedfdc369da5e87afcc",
+    "url": "/cursor-timer/static/js/2.7a95b717.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-timer/static/js/2.7a95b717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f5f03a89d8d86fbcd7",
+    "url": "/cursor-timer/static/js/main.523e8321.chunk.js"
+  },
+  {
+    "revision": "80d4b8a164c6fc4decef",
+    "url": "/cursor-timer/static/js/runtime-main.8109b660.js"
+  }
+]);
